31d7fc71bc5221a7193bbfcc7b61848f35621c0e
Rahulverma143 <rahulvearma17@gmail.com>
2025-12-18 18:23:50 +0530

    update

--- src/in/rahul/dto/Student.java
+++ src/in/rahul/dto/Student.java
@@ -3,28 +3,49 @@ package in.rahul.dto;
 import java.io.Serializable;
 
 public class Student implements Serializable {
-    
+
     private Integer sId;
     private String sname;
     private Integer sage;
     private String saddress;
 
-    public Student() { }
+    public Student() {}
 
-    public Integer getSid() { return sId; }
-    public void setSid(Integer sId) { this.sId = sId; }
+    public Integer getSid() {
+        return sId;
+    }
 
-    public String getSname() { return sname; }
-    public void setSname(String sname) { this.sname = sname; }
+    public void setSid(Integer sId) {
+        this.sId = sId;
+    }
 
-    public Integer getSage() { return sage; }
-    public void setSage(Integer sage) { this.sage = sage; }
+    public String getSname() {
+        return sname;
+    }
 
-    public String getSaddress() { return saddress; }
-    public void setSaddress(String saddress) { this.saddress = saddress; }
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public Integer getSage() {
+        return sage;
+    }
+
+    public void setSage(Integer sage) {
+        this.sage = sage;
+    }
+
+    public String getSaddress() {
+        return saddress;
+    }
+
+    public void setSaddress(String saddress) {
+        this.saddress = saddress;
+    }
 
     @Override
     public String toString() {
-        return "Student [sId=" + sId + ", sname=" + sname + ", sage=" + sage + ", saddress=" + saddress + "]";
+        return "Student [sId=" + sId + ", sname=" + sname +
+               ", sage=" + sage + ", saddress=" + saddress + "]";
     }
 }
